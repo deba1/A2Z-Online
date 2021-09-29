b86913080aeb9124ee5d9893053da24f6473bd8b
Debashish <dsarker515@gmail.com>
2021-09-29 20:54:33 +0600

    Removed unnessesary using statements

--- src/Application/Extensions/ValidationExtensions.cs
+++ src/Application/Extensions/ValidationExtensions.cs
@@ -1,11 +1,6 @@
-﻿using Application.Validators;
+﻿using Application.Validators.AuthenticationDTOValidator;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Extensions
 {
