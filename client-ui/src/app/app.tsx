ab889e10746636ca673dd8c10b920eca3edb4f91
AJakif <akifahmedjahin@gmail.com>
2021-10-08 00:28:40 +0600

    feature/A2ZO-25:Add React Project

---
+++ client-ui/src/app/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function app() {
+  return (
+    <div>
+      Hello World
+      <h1>This is A2Z Client site :3</h1>
+    </div>
+  );
+}
+
+export default app;
---
+++ client-ui/src/components/pages/i.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function i() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default i
