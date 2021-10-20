f62e10cf1cc1bc0cbe02a79cca7563512630276e
AJakif <akifahmedjahin@gmail.com>
2021-10-20 16:57:32 +0600

    A2ZO-27

--- client-ui/src/app/app.tsx
+++ client-ui/src/app/app.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Login from "../components/pages/auth/login/Login";
+import "../assets/css/style.css"
 
 function App() {
   return (
     <div>
-      Hello World
-      <h1>This is A2Z Client site :3</h1>
+      <Login />
     </div>
   );
 }
